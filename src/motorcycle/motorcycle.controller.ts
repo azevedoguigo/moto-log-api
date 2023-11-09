a59d792a34b8ff69c7b528045ff5571a3df31591
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2023-11-09 14:33:11 -0300

    refactor: removing unused methods and making actions asynchronous.

--- src/motorcycle/motorcycle.controller.ts
+++ src/motorcycle/motorcycle.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { MotorcycleService } from './motorcycle.service';
 import { CreateMotorcycleDto } from './dto/create-motorcycle.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
@@ -16,23 +8,13 @@ export class MotorcycleController {
   constructor(private readonly motorcycleService: MotorcycleService) {}
 
   @Post()
-  create(@Body() createMotorcycleDto: CreateMotorcycleDto) {
-    return this.motorcycleService.create(createMotorcycleDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.motorcycleService.findAll();
+  async create(@Body() createMotorcycleDto: CreateMotorcycleDto) {
+    return await this.motorcycleService.create(createMotorcycleDto);
   }
 
   @Get(':id')
   @UseGuards(AuthGuard)
   async findOne(@Param('id') motorcycleId: string) {
     return await this.motorcycleService.findOne(motorcycleId);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.motorcycleService.remove(+id);
   }
 }
